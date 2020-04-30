631f2497ae86a841a798194574541ec890979e62
zheng <1105946494@qq.com>
2020-04-30 19:49:06 +0800

    验证iconPosition为left或right中的一个

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -12446,7 +12446,17 @@ exports.default = void 0;
 //
 //
 var _default = {
-  props: ["icon", "iconPosition"]
+  //   props: ["icon", "iconPosition"],
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: "left",
+      validator: function validator(value) {
+        return value === "left" || value === "right";
+      }
+    }
+  }
 };
 exports.default = _default;
         var $e78980 = exports.default || module.exports;
