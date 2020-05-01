5dc8826a916b816585174b2ae58426594c4b6ecd
zheng <1105946494@qq.com>
2020-05-01 16:28:31 +0800

    安装chai

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -12753,7 +12753,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57194" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61562" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
