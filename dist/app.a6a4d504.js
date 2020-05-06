bc1dd1be43e8ae1291b963a6b2bb1b93fa36483f
zheng <1105946494@qq.com>
2020-05-06 21:42:00 +0800

    让 input 支持 v-model 双向绑定

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -12830,16 +12830,16 @@ exports.default = _default;
         domProps: { value: _vm.value },
         on: {
           change: function($event) {
-            return _vm.$emit("change", $event)
+            return _vm.$emit("change", $event.target.value)
           },
           input: function($event) {
-            return _vm.$emit("input", $event)
+            return _vm.$emit("input", $event.target.value)
           },
           focus: function($event) {
-            return _vm.$emit("focus", $event)
+            return _vm.$emit("focus", $event.target.value)
           },
           blur: function($event) {
-            return _vm.$emit("blur", $event)
+            return _vm.$emit("blur", $event.target.value)
           }
         }
       }),
@@ -12921,7 +12921,8 @@ new _vue.default({
   data: {
     loading1: false,
     loading2: true,
-    loading3: false
+    loading3: false,
+    message: "hi"
   },
   methods: {
     inputChang: function inputChang(e) {
